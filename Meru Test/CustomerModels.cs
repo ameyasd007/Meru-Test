7941c2c9ec790b34a9f7315fad0f851d17f12883
Ameya Dighole <Adighole@realgy.com>
2020-05-07 16:38:31 +0530

    final working commit

---
+++ Meru Test/CustomerModels.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meru_Test
+{
+    public class CustomerDBContext:DbContext
+    {
+        public CustomerDBContext() : base("name=DefaultConnection")
+        {
+            Database.SetInitializer<CustomerDBContext>(new CreateDatabaseIfNotExists<CustomerDBContext>());
+
+
+        }
+
+
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+
+    [Table("Customers")]
+    public class Customer
+    {
+        [Key]
+        public int CustId { get; set; }
+
+        public string CustName { get; set; }
+        public int CustMobNo { get; set; }
+        public string Address { get; set; }
+        [ForeignKey("SubArea")]
+        public int SubAreaId { get; set; }
+
+        public virtual SubArea SubArea {get;set;}
+
+    }
+
+    [Table("SubAreas")]
+    public class SubArea
+    {
+        [Key]
+        public int SubAreaId { get; set; }
+
+        public string SubAreaName { get; set; }
+        [ForeignKey("Area")]
+        public int AreaId { get; set; }
+        public virtual Area Area { get; set; }
+
+    }
+
+    [Table("Areas")]
+    public class Area
+    {
+        [Key]
+        public int AreaId { get; set; }
+
+        public string AreaName { get; set; }
+        [ForeignKey("City")]
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+
+    }
+
+    [Table("Cities")]
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        public string CityName { get; set; }
+        public string State { get; set; }
+    }
+
+
+}
---
+++ Meru Test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meru_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 6, 8, 3, 6, 12, 1, 2, 4, 2, 7, 9, 12, 3, 1, 6, 7, 10, 11 };
+            var MaxSum = HighestSum(arr);
+            Console.WriteLine(MaxSum);
+
+            FindCustomer();
+
+            Console.ReadKey();
+        }
+
+        static int HighestSum(int[] arr)
+        {
+            int MaxSum = 0, CurrentSum = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (i == 0)
+                    CurrentSum = arr[i];
+                ///if current number is a part of current series
+                else if (arr[i]>arr[i-1])
+                {
+                    CurrentSum += arr[i];
+                }
+                ///new ascending series
+                else
+                {
+                    if(CurrentSum > MaxSum)
+                    {
+                        MaxSum = CurrentSum;
+                    }
+                    CurrentSum = arr[i];
+                }
+                //Console.WriteLine($"CurrentSum: {CurrentSum}");
+            }
+
+
+            return MaxSum > CurrentSum ? MaxSum : CurrentSum;
+        }
+
+        static void FindCustomer()
+        {
+            var db = new CustomerDBContext();
+
+            var city = new City() { CityName = "Pune", State = "MH" };
+
+            var area = new Area() { AreaName = "Pashan", City = city };
+
+            var subarea = new SubArea() { Area = area, SubAreaName = "Pune City" };
+
+            var cust = new Customer() { SubArea = subarea, Address = "593", CustMobNo = 1234567890, CustName = "Ameya" };
+
+            db.Customers.Add(cust);
+
+            db.SaveChanges();
+
+            int MobilNumToSearch = 1234567890;
+            var foundCust = db.Customers.Where(c => c.CustMobNo.Equals(MobilNumToSearch)).Select(c => new { c.CustName, c.Address, c.SubArea.SubAreaName, c.SubArea.Area.AreaName, c.SubArea.Area.City.CityName }).FirstOrDefault().ToString();
+
+
+            Console.WriteLine(foundCust);
+        }
+    }
+}
